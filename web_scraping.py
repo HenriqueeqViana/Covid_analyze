ae0396b964edd53068c8b15079f0e3a20aac0fba
HenriqueeqViana <henriquevianaeq@gmail.com>
2020-08-05 01:10:12 -0300

    Mozila rodando em background e melhoria no codigo

--- web_scraping.py
+++ web_scraping.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 import numpy as np
 import matplotlib as plt
@@ -10,17 +11,19 @@ import time
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
-
-	
-driver = webdriver.Firefox()
+from selenium.webdriver.firefox.options import Options
+#Roda o mozila em background.
+options = Options()
+options.add_argument("--headless")
+driver = webdriver.Firefox(firefox_options=options, executable_path="C:\\Users\\henri\\AppData\\Local\\Programs\\Python\\Python38-32\\geckodriver.exe")
+#Inicia o bot no FireFox	
 driver.get('https://covid.saude.gov.br/')
 driver.implicitly_wait(20)
-a= driver.find_element_by_xpath('/html/body/app-root/ion-app/ion-router-outlet/app-home/ion-content/painel-geral-component/div/card-totalizadores-component/div/div[2]/div[2]/div[1]')
+#retira o primeira string com o valor dos casos acumulados da pagina html
+a= driver.find_element_by_xpath('/html/body/app-root/ion-app/ion-router-outlet/app-home/ion-content/painel-geral-component/div/card-totalizadores-component/div/div[2]/div[2]/div[1]').text
 
-html = a.get_attribute("innerHTML") 
-print(html)
-b=driver.find_element_by_xpath('/html/body/app-root/ion-app/ion-router-outlet/app-home/ion-content/painel-geral-component/div/card-totalizadores-component/div/div[3]/div[2]/div[1]')
+print(a)
+#Retira a segunda string com o valor dos dados de mortes da pagina web
+b=driver.find_element_by_xpath('/html/body/app-root/ion-app/ion-router-outlet/app-home/ion-content/painel-geral-component/div/card-totalizadores-component/div/div[3]/div[2]/div[1]').text
 
-html2 = b.get_attribute("innerHTML") 
-print(html2)
-
+print(b)
